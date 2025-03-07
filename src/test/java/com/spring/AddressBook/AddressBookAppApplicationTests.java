85e753674e315f3b2644474c352b687a2f2b1ead
Sanjana Gupta <sanjanagupta1502@gmail.com>
2025-03-07 11:04:18 +0530

    Spring Project is initialized for Address Book App

---
+++ src/test/java/com/spring/AddressBook/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.AddressBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
